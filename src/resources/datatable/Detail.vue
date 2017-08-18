3ffbb60d43c891c41553ff54c3d53fed83465f78
Donovan Broquin <dbroquin@deliverup.com>
2017-08-18 09:36:21 +0200

    Detail
    Add detail component

---
+++ src/resources/datatable/Detail.vue
@@ -0,0 +1,27 @@
+<template>
+    <div @click="onClick">
+        <div class="inline field">
+            <label class="label">{{trans('comments.detail.label')}}</label>
+            <span>{{rowData.content}}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            rowData: {
+                type: Object,
+                required: true
+            },
+            rowIndex: {
+                type: Number
+            }
+        },
+        methods: {
+            onClick (event) {
+                console.log('my-detail-row: on-click', event.target)
+            }
+        },
+    }
+</script>
