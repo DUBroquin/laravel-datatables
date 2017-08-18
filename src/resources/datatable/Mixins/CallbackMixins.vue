6890ef1f3be3ea0e052d761a3f9cf4d8f5ac8d12
Donovan Broquin <dbroquin@deliverup.com>
2017-08-18 09:36:07 +0200

    Callbacks
    Add date and codeName callbacks

--- src/resources/datatable/Mixins/CallbackMixins.vue
+++ src/resources/datatable/Mixins/CallbackMixins.vue
@@ -8,9 +8,14 @@
                 mailTo(content){
                     return '<a class="mailTo" href="mailto:'+content+'">'+content+'</a>'
                 },
+                date(content){
+                    return moment(content, "YYYY-MM-DD HH:MM:SS").format('DD/MM/YYYY HH:MM:SS')
+                },
+                codeName(content){
+                    return content.code + ' - ' + content.name
+                }
             }
         })
-
     }
 </script>
 
