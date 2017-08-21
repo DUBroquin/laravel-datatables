d8cbaa2ff92e161a1018e6f2fe033e68dc6ea983
Donovan Broquin <dbroquin@deliverup.com>
2017-08-21 09:31:29 +0200

    Detail
    Make metod distinct from action (import with SlotsMixins.detailSlot)

--- src/resources/datatable/Mixins/SlotsMixins.vue
+++ src/resources/datatable/Mixins/SlotsMixins.vue
@@ -4,7 +4,11 @@
             methods: {
                 onAction (id, action, data, index) {
                     this.openModal(id, action, data, index)
-                },
+                }
+            }
+        }),
+        detailSlot:({
+            methods: {
                 onDetail(data, id){
                     this.$refs[id].$refs[id].toggleDetailRow(data.id)
                 }
